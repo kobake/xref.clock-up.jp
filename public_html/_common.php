81d165a58081ff91a43ae45762251e3d2b6b2e84
kobake <kobake@users.sourceforge.net>
2014-12-28 20:15:57 +0900

    コメント除去ルーチン整理等

--- public_html/_common.php
+++ public_html/_common.php
@@ -37,6 +37,18 @@ function section($title, $features) {
 	echo "</div>\n";
 }
 
+// コメント除去
+function removeLineComment($line){
+	// 特殊な「#」をマーク
+	$line = preg_replace('/\# (yum|service|mysql)/', '＃ \1', $line);
+	// コメントの除去
+	$line = preg_replace('/\#.*/', '', $line);
+	// 後ろの余計な文字削除
+	$line = preg_replace('/[\t ]+$/', '', $line);
+	// 戻し
+	$line = str_replace('＃', '#', $line);
+	return $line;
+}
 // 展開関数
 function generateContents($text, &$engines, &$features, $default_engines){
 	$engines = array();
@@ -58,9 +70,7 @@ function generateContents($text, &$engines, &$features, $default_engines){
 	for($i = 0; $i < count($lines); $i++){
 		$line = $lines[$i];
 		// コメントの除去
-		$line = preg_replace('/\#.*/', '', $line);
-		// 後ろの余計な文字削除
-		$line = preg_replace('/[\t ]+$/', '', $line);
+		$line = removeLineComment($line);
 		// 空行の無視
 		if($line === '' || $line === $baseIndent){
 			continue;
@@ -97,9 +107,7 @@ function generateContents($text, &$engines, &$features, $default_engines){
 					for ($j = $i + 1; $j < count($lines); $j++) {
 						$line = $lines[$j];
 						// コメントの除去
-						$line = preg_replace('/\#.*/', '', $line);
-						// 後ろの余計な文字削除
-						$line = preg_replace('/[\t ]+$/', '', $line);
+						$line = removeLineComment($line);
 						// 解析
 						if (preg_match("/^{$localIndent}(\t*)([^\\t].*)/", $line, $m)) {
 							$content .= "<br/>" . str_replace("\t", "&nbsp;&nbsp;&nbsp;", $m[1]) . $m[2];
@@ -118,9 +126,7 @@ function generateContents($text, &$engines, &$features, $default_engines){
 				for($j = $i + 1; $j < count($lines); $j++){
 					$line = $lines[$j];
 					// コメントの除去
-					$line = preg_replace('/\#.*/', '', $line);
-					// 後ろの余計な文字削除
-					$line = preg_replace('/[\t ]+$/', '', $line);
+					$line = removeLineComment($line);
 					if(preg_match("/^{$baseIndent}\t(\t*)([^\\t].*)/", $line, $m)){
 						$content .= str_replace("\t", "&nbsp;&nbsp;&nbsp;", $m[1]) . $m[2] . "<br/>";
 					}

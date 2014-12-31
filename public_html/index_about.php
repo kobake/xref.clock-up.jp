f187d2e9a5ab07e06df78cafe4624b5ae24f83a3
kobake <kobake@users.sourceforge.net>
2014-12-31 20:42:53 +0900

    Google Analytics 設定

--- public_html/index_about.php
+++ public_html/index_about.php
@@ -127,5 +127,16 @@ require_once(dirname(__FILE__) . '/_common.php');
 		<script src="/jslib/ie10-viewport-bug-workaround.js"></script>
 		<script src="/metisMenu/dist/metisMenu.min.js"></script>
 		<script src="/js/app.js"></script>
+		
+		<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-8719837-12', 'auto');
+  ga('send', 'pageview');
+
+		</script>
 	</body>
 </html>

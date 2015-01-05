077326fdf68134f5ec95c9d1b37beb1a2acb19c8
kobake <kobake@users.sourceforge.net>
2015-01-05 22:09:29 +0900

    不要コード削除

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -61,43 +61,6 @@ jQuery(function(){
 	$('li.parent-li').addClass('active');
 	$('ul.nav-second-level').collapse('show');
 	*/
-	
-	return; // もう以下は必要無い。
-	jQuery('td').each(function(){
-		var td = jQuery(this);
-		var html = td.html();
-		// 初期改行除去
-		html = html.replace(/^\n/, "");
-
-		// 最終改行除去
-		html = html.replace(/\n\t+$/i, "");
-
-		// 一旦配列にする
-		var lines = html.split(/\n/);
-
-		// 初期インデント -> baseIndent
-		var baseIndent = '';
-		if(lines.length > 0){
-			var m = lines[0].match(/^\t+/);
-			if(m){
-				baseIndent = m[0];
-			}
-		}
-
-		// 構築し直し
-		var re = new RegExp("^" + baseIndent);
-		html = '';
-		lines.forEach(function(line){
-			line = line.replace(re, "");
-			line = line.replace(/\t/g, "&nbsp;&nbsp;&nbsp;&nbsp;");
-			html += line + "<br/>\n";
-		});
-
-		// html = html.replace(/\n/g, "<br/>\n");
-		//
-		// 適用
-		td.html(html);
-	});
 });
 
 function mytext(str){

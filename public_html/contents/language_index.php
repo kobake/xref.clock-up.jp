488c8fa40acae341ce2b534dccd1eb91ea29fd9c
kobake <kobake@users.sourceforge.net>
2015-01-08 00:18:02 +0900

    リテラルとか定数とか諸々

--- public_html/contents/language_index.php
+++ public_html/contents/language_index.php
@@ -30,31 +30,98 @@ $smarty->assign('contents', $contents);
   ]
   );
  */
-section('run:起動', [
+section('engine:エンジン/run:起動', [
 	'対話起動',
 	]
 );
 
-section('logic:論理演算', [
+// 変数
+section('variable:変数・定数/declare:変数宣言', [
+	'変数宣言',
+	'グローバル変数',
+	'局所変数',
+	]
+);
+section('variable:変数・定数/literal:整数リテラル', [
+	'10進',
+	'16進',
+	'8進',
+	'2進',
+	]
+);
+section('variable:変数・定数/const:定数宣言', [
+	'定数宣言',
+	]
+);
+section('variable:変数・定数/string:文字列', [
+	'リテラル',
+	'結合',
+	]
+);
+
+// 出力
+section('io:入出力/standard:標準入出力', [
+	'標準出力',
+	'標準入力',
+	]
+);
+section('io:入出力/file:ファイル入出力', [
+	'ファイル出力',
+	'ファイル入力',
+	]
+);
+
+// 演算
+section('calc:演算/logic:論理演算', [
 	'論理OR',
 	'論理AND',
 	'論理NOT',
 	]
 );
-section('bit:ビット演算', [
+section('calc:演算/bit:ビット演算', [
 	'ビットOR',
 	'ビットAND',
 	'ビットNOT',
 	'ビットXOR',
 	]
 );
-section('bool:BOOL', [
+section('calc:演算/bool:BOOL', [
 	'True定数',
 	'False定数',
 	'True判定',
 	'False判定',
 	]
 );
+
+// 制御構造
+section('control:制御/cond:分岐', [
+	'if分岐',
+	'switch分岐',
+	]
+);
+section('control:制御/floop:For系ループ', [
+	'Forループ',
+	'ForEachループ',
+	]
+);
+section('control:制御/wloop:While系ループ', [
+	'Whileループ',
+	'DoWhileループ',
+	]
+);
+section('control:制御/jump:ジャンプ', [
+	'ループ抜け',
+	'ループ継続',
+	]
+);
+section('control:制御/exception:例外', [
+	'例外送出',
+	'例外Catch',
+	]
+);
+
+
+
 /*
 section('logic2:演算まとめ', [
 	'論理演算',
@@ -65,30 +132,11 @@ section('logic2:演算まとめ', [
 */
 
 
-// 閲覧
-section('variable:変数', [
-	'変数宣言',
-	'グローバル変数',
-	]
-);
 section('invalid:無効値', [
 	'無効値',
 	'無効判定',
 	]
 );
 
-// 制御構造
-section('control:制御構造', [
-	'if分岐',
-	'switch分岐',
-	'whileループ',
-	'回数ループ'
-	]
-);
 
-// 出力
-section('output:出力', [
-	'Hello出力',
-	]
-);
 
